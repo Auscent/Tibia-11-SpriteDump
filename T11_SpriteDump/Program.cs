a0129ab7ec48269bc561653ad46a8e5c1f4101af
Auscent <auscent.xxx@gmail.com>
2017-11-26 19:14:43 +1100

    Updated header handling to support newer format

--- T11_SpriteDump/Program.cs
+++ T11_SpriteDump/Program.cs
@@ -124,13 +124,23 @@ namespace T11_SpriteDump
 			{
 				Stream input = reader.BaseStream;
 				
-				// CIP's header
-				input.Position = 6; // Skip past 6 initial constant bytes
+				/*
+  					CIP's header, 32 bytes
+				
+					Padded with a variable number of NULL bytes (depending on LZMA file size) at the start then the constant
+					byte sequence of "70 0A FA 80 24" followed by the LZMA file size encoded as a 7-bit integer
+				*/
+				
+				// Since there may be a variable number of NULL bytes just loop until we get through them all, then skip past
+				// the remaining 4 bytes of the constant (really should check their values for file validity but just assume it is)
+				while (reader.ReadByte() == 0) { }
+				reader.ReadUInt32();
+				
 				while ((reader.ReadByte() & 0x80) == 0x80) { } // LZMA size, 7-bit integer where MSB = flag for next byte used
 				
 				// LZMA file
 				decoder.SetDecoderProperties(reader.ReadBytes(5));
-				reader.ReadUInt64();
+				reader.ReadUInt64(); // Should be the decompressed size but CIP writes the compressed sized, so just use a large buffer size
 				
 				// Disabled arithmetic underflow/overflow check in debug mode so this won't cause an exception
 				spriteBuffer.Position = 0;
